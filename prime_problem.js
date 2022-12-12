37117af3913a0e105357cf63254b1a010b23cb79
unknown <devanshsahuyanim@gmail.com>
2022-12-13 00:59:37 +0530

    fixed

--- prime_problem.js
+++ prime_problem.js
@@ -1,12 +1,13 @@
 //Prime_Problem
 let n=5,count=0;
-for(let i=0;i>=n;i--){
-if(n%1!=0){
+for(let i=0;i<=n;i++){
+if(n%1==2){
 count++;
 }
 }
-if(count==0){
+if(count==2){
 console.log("yes")
 }
 else{
 console.log("no")
+}
